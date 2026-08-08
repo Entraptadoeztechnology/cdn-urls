5f2e1be44f9edcabe1a19f77fd3ba99489961ef6
Martin_MMC <martin.poliyanskiy+github@gmail.com>
2026-08-08 14:26:27 -0700

    Add usage comments to CDN URLs in cdns.js
    Updated URLs in cdns.js to include comments indicating the format for usage.

---
+++ cdns.js
@@ -0,0 +1,28 @@
+const jsdelivrUrls = [ 
+    "https://cdn.jsdelivr.net/", // gh/username/repo@branch/filepath
+    "https://fastly.jsdelivr.net/",
+    "https://gcore.jsdelivr.net/",
+    "https://quantil.jsdelivr.net/",
+    "https://originfastly.jsdelivr.net/",
+    "https://testingcf.jsdelivr.net/",
+];
+
+const staticallyUrls = [
+    "https://cdn.statically.io/" // gh/username/repo@branch/filepath
+];
+
+const staticdelivrUrls = [
+    "https://cdn.staticdelivr.com/" // gh/username/repo/branch/filepath
+];
+
+const gitmirrorUrls = [
+    "https://raw.gitmirror.com/" // username/repo/branch/filepath
+];
+
+const githackUrls = [
+    "https://raw.githack.com/" // username/repo/branch/filepath
+];
+
+const githubusercontentUrls = [
+    "https://raw.githubusercontent.com/" // username/repo/branch/filepath
+];
